ec3cb80e33696433e4d4406ba8790e8b69624e3a
t9md <taqumd@gmail.com>
2020-04-20 22:13:02 +0900

    up

--- add_img_field.rb
+++ add_img_field.rb
@@ -2,4 +2,7 @@ File.readlines(ARGV[0]).each do |line|
   fields = line.chomp.split(/\t/)
   fields.push %!<img src="google-img--#{fields[0]}.jpg">!
   puts fields.join("\t")
+
+  # fields.insert(-1, %!<img src="google-img--#{fields[2]}.jpg">!)
+  # puts fields.join("\t") + "\t"
 end
